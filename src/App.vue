f417ca5081d5a45860b40df989a44b4b557645b5
Slixe <slixeprivate@gmail.com>
2019-12-30 14:33:16 +0100

    Navbar + footer

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,28 @@
     <v-app>
       <header>
           <v-toolbar dark>
+            <v-spacer></v-spacer>
             <v-toolbar-side-icon>
-              <v-img src="/logo.png" height="50px" width="50px"> 
+              <v-img src="/logo.png" height="60px" width="60px"> 
               </v-img>
             </v-toolbar-side-icon>
-            <v-toolbar-title>DERO Explorer</v-toolbar-title>
+            <v-btn to="/" text>
+              <v-toolbar-title>DERO Explorer</v-toolbar-title>
+            </v-btn>
             <v-spacer></v-spacer>
-                  <v-text-field
+            <v-text-field
               hide-details
               prepend-icon="search"
               single-line
+              label = "Block height, block hash, transaction hash"
             ></v-text-field>
             <v-spacer></v-spacer>
             <v-toolbar-items class="hidden-sm-and-down">
-              <v-btn to="/products" text>Home</v-btn>
-              <v-btn href="http://stats.dero.io" text>Web Wallet</v-btn>
+              <v-btn href="https://dero.io" text>Home</v-btn>
+              <v-btn href="https://wallet.dero.io" text>Web Wallet</v-btn>
               <v-btn href="http://stats.dero.io" text>Stats</v-btn>
             </v-toolbar-items>
+            <v-spacer></v-spacer>
           </v-toolbar>
         </header>
 
@@ -28,6 +33,33 @@
               <router-view></router-view>
             </transition>
         </v-content>
+
+        <v-footer padless>
+          <v-card
+            flat
+            tile
+            width="100%"
+            dark
+            class="text-center"
+          >
+            <!--<v-card-text> 
+              <v-btn
+                v-for="icon in icons"
+                :key="icon"
+                class="mx-4"
+                icon
+              >
+                <v-icon size="24px">{{ icon }}</v-icon>
+              </v-btn>
+            </v-card-text>
+
+            <v-divider></v-divider>-->
+
+            <v-card-text class="white--text">
+              {{ new Date().getFullYear() }} — <strong>DERO Explorer</strong>
+            </v-card-text>
+          </v-card>
+        </v-footer>
       </v-app>
   </div>
 </template>
