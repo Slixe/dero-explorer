460956712f647e3b9471210572937a76d76e5a6f
Slixe <slixeprivate@gmail.com>
2020-01-05 10:04:57 +0100

    Polyfill

--- src/wasm.js
+++ src/wasm.js
@@ -4,19 +4,16 @@ const go = new Go();
 
 export async function useWASM()
 {
+    if (!WebAssembly.instantiateStreaming) { // polyfill
+        WebAssembly.instantiateStreaming = async (resp, importObject) => {
+            const source = await (await resp).arrayBuffer();
+            return await WebAssembly.instantiate(source, importObject);
+        };
+    }
+
     let result = await WebAssembly.instantiateStreaming(fetch("/main.wasm"), go.importObject)
     go.run(result.instance);
     console.log(testWASM())
-    /*let minerTx = 
-    console.log("Miner tx from wasm is " + minerTx)
-    console.log("Txs in block: " + block.json.tx_hashes)
-    explorer.loadTxs(block.json.tx_hashes).then(result => {
-        console.log(result)
-        let test = wasm.deserializeTx(JSON.stringify(result))
-
-        if (test)
-            console.log(JSON.parse(test))
-    })*/
 }
 
 export function addMinerTxToBlock(block)
